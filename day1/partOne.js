60114d319e38bc4405b5776a844f3022bbe96d4d
JohnKougioum <jkougioumtzidis@gmail.com>
2023-12-11 02:12:32 +0200

    remove consoles

--- day1/partOne.js
+++ day1/partOne.js
@@ -2,10 +2,8 @@ import fs from "fs";
 
 const input = fs.readFileSync("./input.txt", "utf-8").trim().split("\r\n");
 
-console.time();
 let result = 0;
 for (const line of input) {
   const numberArray = line.split('').filter((char) => /\d/.test(char)); // Match digits with \d and filter out non-digits. This is faster that during something like !isNaN(parseInt(char))
   result += parseInt(numberArray[0] + numberArray[numberArray.length - 1]);
 }
-console.timeEnd();
